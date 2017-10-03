9c38d5ce10f44a66beb7b4a276fa193dfecb135a
Dom De Re <domdere@domdere.com>
2017-10-03 23:09:58 +1100

    bitbucket-http-client
    - HTTP API calls for the bitbucket api
    - only the "list repos" call at the moment

---
+++ bitbucket-http-client/src/Irreverent/Bitbucket/Http.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+-------------------------------------------------------------------
+-- |
+-- Module       : Irreverent.Bitbucket.Http
+-- Copyright    : (C) 2017 Irreverent Pixel Feats
+-- License      : BSD-style (see the file /LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-------------------------------------------------------------------
+module Irreverent.Bitbucket.Http (module X) where
+
+import Irreverent.Bitbucket.Http.Repositories.List as X
