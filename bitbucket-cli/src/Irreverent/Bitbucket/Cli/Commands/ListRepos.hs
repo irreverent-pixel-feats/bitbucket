8d40625fede51ed012e02d4443c861d1bf3c9d83
Dom De Re <domdere@domdere.com>
2018-02-23 14:49:50 +1100

    bitbucket-cli
    - Changed the list repo command so that the list of repos can be streamed

--- bitbucket-cli/src/Irreverent/Bitbucket/Cli/Commands/ListRepos.hs
+++ bitbucket-cli/src/Irreverent/Bitbucket/Cli/Commands/ListRepos.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
@@ -16,30 +17,49 @@ module Irreverent.Bitbucket.Cli.Commands.ListRepos (
 
 import Irreverent.Bitbucket.Cli.Error
 
-import Irreverent.Bitbucket.Core (Username(..), RepoDescription(..), RepoName(..), Repository(..), User(..))
+import Irreverent.Bitbucket.Core (
+    Username(..)
+  , Project(..)
+  , ProjectName(..)
+  , RepoDescription(..)
+  , RepoName(..)
+  , Repository(..)
+  )
 import Irreverent.Bitbucket.Core.Control (BitbucketT(..))
 import Irreverent.Bitbucket.Core.Data.Auth (Auth(..))
 import Irreverent.Bitbucket.Http.Error (BitbucketAPIError)
-import Irreverent.Bitbucket.Http.Repositories.List (listRepositories)
+import Irreverent.Bitbucket.Http.Repositories.List (repoConduit)
 
 import Ultra.Control.Monad.Catch (MonadCatch)
 import Ultra.Control.Monad.Trans.Either (EitherT, firstEitherT, mapEitherT)
-import Ultra.Data.List.NonEmpty (sortBy)
+import Ultra.Data.Conduit (Conduit, ($$), (=$=), awaitForever,  yield)
+import qualified Ultra.Data.Conduit.List as C
+import qualified Ultra.Data.Conduit.Binary as C
 import qualified Ultra.Data.Text as T
+import qualified Ultra.Data.Text.Encoding as T
 
-import Data.List (replicate)
+import qualified Data.ByteString as BS
 
 import qualified Network.Wreq.Session as S
 
-import System.IO (hPutStr, hPutStrLn, stdout)
-
-import Text.Printf (printf)
+import System.IO (stdout)
 
 import Preamble
 
 data ListReposError =
   ListRepoAPIFail !BitbucketAPIError
   deriving (Show)
+
+renderPipe
+  :: (Monad m)
+  => Conduit Repository m BS.ByteString
+renderPipe = C.map $ \r -> T.encodeUtf8 . T.concat $ [
+    getRepoName . repoName $ r
+  , maybe "" (T.bracketed " (" ")" . getProjectName . projectName) . repoProject $ r
+  , "    : "
+  , getDescription . repoDescription $ r
+  , "\n"
+  ]
 
 listRepos
   :: (MonadCatch m, MonadIO m)
@@ -48,31 +68,4 @@ listRepos
   -> EitherT CliError m ()
 listRepos auth owner = firstEitherT BitbucketAPIFail . mapEitherT (flip runReaderT auth . runBitbucketT) $ do
   session <- liftIO S.newSession
-  repos <- listRepositories session owner
-  let h = printf (T.unpack $ formatString repos) (t "creator") (t "slug") (t "description")
-  let b = replicate (length h - 1) '-'
-  liftIO $ hPutStr stdout h >> hPutStrLn stdout b
-  forM_ repos $ \repo -> liftIO $
-      printf
-        (T.unpack $ formatString repos)
-        (maybe "None" (getUsername . userName) . repoCreator $ repo)
-        (getRepoName . repoName $ repo)
-        (getDescription . repoDescription $ repo)
-  -- runEitherT . mapEitherT (flip runReaderT auth . runBitbucketT) $ do
-  --   repoConduit session owner $$ listSink
-
-biggestLengthBy :: [a] -> (a -> Int) -> Int -> Int
-biggestLengthBy xs f def = maybe def (head . sortBy (flip compare)) . nonEmpty . fmap f $ xs
-
-columnFormatSpecifier :: Int -> T.Text
-columnFormatSpecifier n = T.concat [" %-", T.pack . show $ n, "s "]
-
-formatString :: [Repository] -> T.Text
-formatString rs = flip T.snoc '\n' . T.intercalate "|" . fmap (columnFormatSpecifier . uncurry (biggestLengthBy rs) . fmap T.length) $ [
-    (T.length . maybe "None" (getUsername . userName) . repoCreator, "creator")
-  , (T.length . getRepoName . repoName, "slug")
-  , (T.length . getDescription . repoDescription, "description")
-  ]
-
-t :: T.Text -> T.Text
-t = id
+  (repoConduit session owner =$= awaitForever (traverse yield) =$= renderPipe $$ C.sinkHandle stdout)
